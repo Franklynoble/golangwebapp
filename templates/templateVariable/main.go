9624025e00957f79b2309f9a91d4608e29eec651
Franlky01 <Franklynoblez@gmail.com>
2021-08-15 08:51:40 +0100

    template variable

--- templates/templateVariable/main.go
+++ templates/templateVariable/main.go
@@ -14,8 +14,9 @@ func init() {
 func main() {
 	//we can also pass in aggregate data
 	// using the dot notation to parse data and you can only parse one p piece of data
-	err := tpl.ExecuteTemplate(os.Stdout,"tpl.gohtml",42)
-   if err != nil {
-   	log.Fatalln(err)
-   }
+	err := tpl.ExecuteTemplate(os.Stdout,"tpl.gohtml",`release self-focus; embrace other-focus`)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
+
