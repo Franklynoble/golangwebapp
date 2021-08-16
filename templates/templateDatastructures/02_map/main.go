216919ce8f291bc7e115d43f4cf6e68c89d11586
Franlky01 <Franklynoblez@gmail.com>
2021-08-16 02:39:13 +0100

    Add map file 02_map

---
+++ templates/templateDatastructures/02_map/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+var tpl *template.Template
+func init() {
+	tpl = template.Must(template.ParseFiles("go.gohtml"))
+}
+
+func main() {
+
+	sages := map[string]string{
+	"India": "Gandhi",
+	 "America":"Milk",
+	  "Mediate":"Budha",
+	   "Love": "Jesus",
+	   "Prohpet":"Muhammed",
+	}
+
+ if 	err := tpl.Execute(os.Stdout,sages); err != nil {
+ 	     log.Fatalln(err)
+ }
+
+
+
+}
